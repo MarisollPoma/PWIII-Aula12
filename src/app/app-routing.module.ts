19884576850daebcb0501df3fa17789c45bb88b1
Marisol Poma <marisol.poma15@hotmail.com>
2019-10-11 22:23:50 -0300

    PWIII- Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProdutosComponent} from './produtos/produtos.component';
+import {NovoProdutoComponent} from './novo-produto/novo-produto.component';
+import{ContentComponent} from'./content/content.component';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
-const routes: Routes = [];
+/*const aqui adicionaremos links  para direcionar a uma coluna do site*/
+const routes: Routes = [
+  {
+    path : 'produto', 
+    component : ProdutosComponent
+  },
+
+  {
+      path : 'novo-produto', 
+      component : NovoProdutoComponent
+  },
+
+  {
+    path : '', 
+    component : ContentComponent  
+  }
+
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
